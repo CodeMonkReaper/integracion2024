46eeee7e42f3c325ea3c5fc7eea66f2c8a4d39e4
Andokai <da.nova@duocuc.cl>
2024-07-02 15:03:06 -0400

    Avance dia 7

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,13 +1,15 @@
 import os
-from flask import Flask, render_template, request, url_for, redirect
+import json
+import urllib.request
+from flask import Flask, render_template, request, url_for, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import event, DDL
+from sqlalchemy import event, DDL, Column, Integer, String, Numeric, DateTime, func
 from datetime import datetime
 from flask_bcrypt import generate_password_hash, check_password_hash
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://administrador:NCPgatauFcE4HfYylftN8tX7YAyfNbSv@dpg-cp74hngl6cac7386tmmg-a.oregon-postgres.render.com/ferremas_6a85'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://administrador:Ld8vp14eh4UdF6H9YpN5bkHe5CzjUkqv@dpg-cq1pbs2ju9rs73bbo3p0-a.oregon-postgres.render.com/ferremas_ersz'
 db = SQLAlchemy(app)
 
 @app.route("/flask", methods=['GET'])
@@ -16,13 +18,13 @@ def index():
 
 class Venta(db.Model):
     __tablename__ = 'venta'
-    id_venta = db.Column(db.Integer, primary_key=True)
+    id_venta = db.Column(db.Integer, primary_key=True, autoincrement=True)
     precio = db.Column(db.Numeric(10, 2), nullable=False)
     cantidad = db.Column(db.Integer, nullable=False)
 
 class Producto(db.Model):
     __tablename__ = 'producto'
-    id_producto = db.Column(db.Integer, primary_key=True)
+    id_producto = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(100), nullable=False)
     descripcion = db.Column(db.String(255))
     precio = db.Column(db.Numeric(10, 2), nullable=False)
@@ -42,7 +44,7 @@ class HistorialPrecioProducto(db.Model):
 
 class Inventario(db.Model):
     __tablename__ = 'inventario'
-    id_inventario = db.Column(db.Integer, primary_key=True)
+    id_inventario = db.Column(db.Integer, primary_key=True, autoincrement=True)
     id_producto = db.Column(db.Integer, db.ForeignKey('producto.id_producto'), nullable=False)
     id_sucursal = db.Column(db.Integer, db.ForeignKey('sucursal.id_sucursal'), nullable=False)
     cantidad = db.Column(db.Integer, nullable=False)
@@ -81,19 +83,59 @@ class Vendedor(db.Model):
     id_vendedor = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
     rol_id = db.Column(db.Integer, db.ForeignKey('rol.id'), nullable=False)
     id_sucursal = db.Column(db.Integer, db.ForeignKey('sucursal.id_sucursal'))
 
-    def __init__(self, nombre, email, password, rol_id):
-        self.nombre = nombre
-        self.email = email
-        self.set_password(password)
-        self.rol_id = rol_id
+class Chat(db.Model):
+    __tablename__ = 'chat'
+    id_mensaje = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    mensaje = db.Column(db.String(100), nullable=False)
+    rol = db.Column(db.Integer, db.ForeignKey('rol.id'))
+    id_chat = db.Column(db.Numeric(10, 2), nullable=False)
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+class TasaCambio(db.Model):
+    __tablename__ = 'tasas_cambio'
+    moneda_destino = Column(String(3), primary_key=True)
+    tasa = Column(Numeric(18, 6), nullable=False)
+    fecha_actualizacion = Column(DateTime, default=func.now(), nullable=False)
 
+def obtener_tasas_desde_api():
+    api_url = 'https://api.exchangerate-api.com/v4/latest/CLP' 
+    try:
+        with urllib.request.urlopen(api_url) as response:
+            data = json.loads(response.read())
+            tasas = data['rates']
+            return tasas
+    except Exception as e:
+        print(f"Error al obtener las tasas de cambio desde la API: {e}")
+        return None
+
+def actualizar_tasas_cambio():
+    tasas = obtener_tasas_desde_api()
+    try:
+        if tasas:
+            for moneda, tasa in tasas.items():
+                tasa_cambio = TasaCambio.query.filter_by(moneda_destino=moneda).first()
+                if tasa_cambio:
+                    tasa_cambio.tasa = tasa
+                else:
+                    tasa_cambio = TasaCambio(moneda_destino=moneda, tasa=tasa)
+                    db.session.add(tasa_cambio)
+            
+            db.session.commit()
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(f"Error al actualizar las tasas de cambio: {e}")
+        db.session.rollback()
+        return False
+
+@app.route('/actualizar-tasas-cambio', methods=['GET'])
+def ruta_actualizar_tasas_cambio():
+    if actualizar_tasas_cambio():
+        return jsonify({'message': 'Tasas de cambio actualizadas correctamente.'}), 200
+    else:
+        return jsonify({'message': 'Error al actualizar las tasas de cambio.'}), 500
--- backend/app/config.py
+++ backend/app/config.py
@@ -3,6 +3,6 @@ import os
 class Config:
     # Configuración de la base de datos
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://administrador:NCPgatauFcE4HfYylftN8tX7YAyfNbSv@dpg-cp74hngl6cac7386tmmg-a.oregon-postgres.render.com/ferremas_6a85'
+        'postgresql://administrador:Ld8vp14eh4UdF6H9YpN5bkHe5CzjUkqv@dpg-cq1pbs2ju9rs73bbo3p0-a.oregon-postgres.render.com/ferremas_ersz'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'clavesecreta'
